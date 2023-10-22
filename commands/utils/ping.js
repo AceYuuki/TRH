0d7c7a739a46a30b73df07733d3673365ef7f3aa
Yuuki <yuukiskiiy@gmail.com>
2023-10-22 18:01:35 +0200

    heroku

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,35 +1,109 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js');
+const { readdirSync } = require('fs');
+const commandFolder = readdirSync('./commands');
+
+
+const contextDescription = {
+    userinfo: `Renvoie des infos sur l'utilisateur`
+};
+
 module.exports = {
-    name: 'ping',
+    name: 'help',
     category: 'utils',
     permissions: ['SEND_MESSAGES'],
     ownerOnly: false,
-    usage: 'ping',
-    exemples: ['ping'],
-    description: 'Affiche la latence du bot et de l\'API',
-    async run(client, message, args) {
-        const tryPong = await message.channel.send('Calcule de la latence en cours ... ')
+    usage: 'help',
+    exemples: ['help', 'help ping', 'help emit'],
+    description: 'commande help',
+     run(client, message, args, guildSettings) {
+        const prefix = guildSettings.prefix;
+        
+        if (!args.length){
+            const noArgsEmbed1 = new MessageEmbed()
+            .setColor('BLUE')
+            .addField('__Liste des commandes__', `Une liste de toutes les catégories disponible et leurs commands\nPour plus d'informations sur une commande , taper \`${prefix}help [commande]\`\n** **`)
+     
+            for(const category of commandFolder){
+                noArgsEmbed1.addField( 
+                    `|__${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}:__`,
+                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``, true
+                     );
+            };
+            return message.channel.send({ embeds: [noArgsEmbed1] });
+        };
+        const cmd = client.commands.get(args[0]);
+        if (!cmd) return message.reply(`Cette commande n'existe pas !`);
 
-        const setupEmbed = new MessageEmbed();
-        setupEmbed.setColor('BLUE');
-        setupEmbed.setAuthor({ name: `${client.user.username}#${client.user.discriminator}`});
-        setupEmbed.addFields(
-            {name: 'Latence API', value:`**\`\`\`${client.ws.ping}ms\`\`\`** `, inline: true},
-            {name: "Latence BOT", value: `**\`\`\`${tryPong.createdTimestamp - message.createdTimestamp}ms\`\`\`**`, inline: true}
-        );
-        setupEmbed.setThumbnail(`${client.user.displayAvatarURL({ format: 'png', dynamic: true })}`)
-        tryPong.edit({content: '  ', embeds: [setupEmbed]});
+        const argsEmbed = new MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`**__${cmd.name.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}__**`)
+        .setDescription(` \`\`\`makefile
+[Help Commande -> ${cmd.name}] ${cmd.ownerOnly ? '/!\\ Pour les admins du bot uniquement /!\\' : ''}
+
+${cmd.description ? cmd.description : contextDescription[`${cmd.name}`]}
+
+Permissions: ${cmd.permissions.join(', ')}
+Utilisation: ${prefix}${cmd.usage}
+Exemples: ${prefix}${cmd.exemples.join(` | ${prefix}`)}
+
+---
+
+${prefix} = prefix utilisé pour le bot (/commands sont aussi disponibles)
+{} = sous-commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) facultatif(s)
+Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.
+\`\`\` `)
+        return message.channel.send({ embeds: [argsEmbed]});
     },
-    async runInteraction(client, interaction) {
-        const tryPong = await interaction.reply({ content:'Calcule de la latence en cours ... ', fetchReply: true })
-        const setupEmbed = new MessageEmbed();
-       setupEmbed.setColor('#2f3136');
-        setupEmbed.setAuthor({ name: `${client.user.username}#${client.user.discriminator}`});
-        setupEmbed.addFields(
-            {name: 'Latence API', value:`**\`\`\`${client.ws.ping}ms\`\`\`** `, inline: true},
-            {name: "Latence BOT", value: `**\`\`\`${tryPong.createdTimestamp - interaction.createdTimestamp}ms\`\`\`**`, inline: true}
-        );
-        setupEmbed.setThumbnail(`${client.user.displayAvatarURL({ format: 'png', dynamic: true })}`)
-        interaction.editReply({content: '  ', embeds: [setupEmbed]});
+    options: [
+        {
+        name: 'command',
+        description: 'tapez le nom de votre commande',
+        type: 'STRING',
+        required: false,
+        }
+    ],
+    runInteraction(client, interaction, guildSettings){
+        const cmdName = interaction.options.getString('command');
+        const prefix = guildSettings.prefix;
+
+        if (!cmdName){
+            const noArgsEmbed = new MessageEmbed()
+            .setColor('BLUE')
+            .addField('__Liste des commandes__', `Une liste de toutes les catégories disponible et leurs commands\nPour plus d'informations sur une commande , taper \`${prefix}help [commande]\`\n** **`)
+     
+            for(const category of commandFolder){
+                noArgsEmbed.addField( 
+                    `|__${category.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}:__`,
+                    `\`${client.commands.filter(cmd => cmd.category == category.toLowerCase()).map(cmd => cmd.name).join(', ')}\``,true
+                     );
+            }
+            return interaction.reply({ embeds: [noArgsEmbed], ephemeral: true});
+        }
+        const cmd = client.commands.get(cmdName);
+        if (!cmd) return interaction.reply({content: `Cette commande n'existe pas !`, ephemeral: true });
+
+        const argsEmbed = new MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`**__${cmd.name.replace(/(^\w|\s\w)/g, firstLetter => firstLetter.toUpperCase())}__**`)
+        .setDescription(`
+        
+\`\`\`makefile
+[Help Commande -> ${cmd.name}] ${cmd.ownerOnly ? '/!\\ Pour les admins du bot uniquement /!\\' : ''}
+
+${cmd.description ? cmd.description : contextDescription[`${cmd.name}`]}
+
+Permissions: ${cmd.permissions.join(', ')}
+Utilisation: ${prefix}${cmd.usage}
+Exemples: ${prefix}${cmd.exemples.join(` | ${prefix}`)}
+
+---
+
+${prefix} = prefix utilisé pour le bot (/commands sont aussi disponibles)
+{} = sous-commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) facultatif(s)
+Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.
+\`\`\`
+        
+        `)
+        return interaction.reply({ embeds: [argsEmbed], ephemeral: true});
     }
 }

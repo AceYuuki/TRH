ec4e3dd0c759cc356acf9c25d31205b5005b4526
Yuuki <yuukiskiiy@gmail.com>
2023-12-17 02:22:17 +0100

    heroku

---
+++ commands/modération/createrole.js
@@ -0,0 +1,55 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'lock',
+    category: 'modération',
+    permissions: ['MANAGE_CHANNELS'],
+    ownerOnly: false,
+    usage: 'lock',
+    exemples: ['lock'],
+    description: 'lock un channel',
+     async run(client, message, args) {
+        const roleName = message.content.split(" ")[1];
+        if (!roleName) {
+        message.channel.send("Please specify a role name.");
+        return;
+        }
+
+        try {
+        const role = await message.guild.roles.create({
+            name: roleName,
+            color: 'BLUE', // Vous pouvez changer la couleur ici
+            reason: 'Role needed for XYZ'
+        });
+
+        message.channel.send(`Role ${role.name} created successfully!`);
+        } catch (error) {
+        console.error(error);
+        message.channel.send("An error occurred while creating the role.");
+        }
+    },
+    options: [
+        {
+            name: 'name',
+            description: 'Nom du rôle',
+            type: 'STRING',
+            required: true,
+        },
+    ],
+    async runInteraction(client, interaction){
+        const roleName = interaction.options.getString('name');
+
+        try {
+        const role = await interaction.guild.roles.create({
+            name: roleName,
+            color: 'BLUE', // Vous pouvez changer la couleur ici
+            reason: 'Role needed for XYZ'
+        });
+
+        await interaction.reply(`Role ${role.name} created successfully!`);
+        } catch (error) {
+        console.error(error);
+        await interaction.reply("An error occurred while creating the role.");
+        }
+    }
+}
